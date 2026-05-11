4eaff9caf1205ffea86e1c0f56838d592ab9da54
edwardcarvalho <edward.silva@mixmove.pt>
2026-05-11 12:52:48 +0100

    added google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8340172955044180"
+          crossOrigin="anonymous"
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
